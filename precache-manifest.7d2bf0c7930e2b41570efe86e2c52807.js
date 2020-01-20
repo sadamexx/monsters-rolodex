b020ed61d77e87a988196f5f7e614a7f67c464f1
Sarah Elias <sarah@Sarahs-MacBook-Pro.local>
2020-01-20 11:39:06 -0800

    Updates

---
+++ precache-manifest.7d2bf0c7930e2b41570efe86e2c52807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91b957edbf130433a73ca0dc3ce43921",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "e20d804b098a123cd6d2",
+    "url": "/monsters-rolodex/static/css/main.3a310ace.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e20d804b098a123cd6d2",
+    "url": "/monsters-rolodex/static/js/main.b2c1525b.chunk.js"
+  },
+  {
+    "revision": "517fd9319d43d3a57612",
+    "url": "/monsters-rolodex/static/js/runtime-main.ae75c310.js"
+  }
+]);
